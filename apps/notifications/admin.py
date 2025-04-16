f1fe945212d5dcd659dfe8e270c16a31d03f5efb
globalbuilder <globalbuilder777@gmail.com>
2025-04-16 21:18:35 +0300

    Notifications 2

--- apps/notifications/admin.py
+++ apps/notifications/admin.py
@@ -1,11 +1,8 @@
-# apps/notifications/admin.py
-
 from django.contrib import admin
 from .models import Notification
 
 @admin.register(Notification)
 class NotificationAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'is_broadcast', 'user', 'created_at')
-    list_filter = ('is_broadcast', 'user')
-    search_fields = ('title', 'message', 'user__username')
-    ordering = ('-created_at',)
+    list_display = ('id', 'user', 'title', 'is_read', 'created_at')
+    search_fields = ('user__username', 'title', 'message')
+    list_filter = ('is_read', 'created_at')
